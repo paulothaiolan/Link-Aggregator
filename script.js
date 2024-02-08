f813062aba5f65b75877da7f8b7174c6fe64a9b9
paulothaiolan <159341135+paulothaiolan@users.noreply.github.com>
2024-02-08 13:05:05 -0300

    Alt Bot

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
 function toggleMode() {
-  const html = document.documentElement
-  html.classList.toggle("light")
+    const html = document.documentElement
+    html.classList.toggle("light")
 
-  //pegar a tag img
+    //pegar a tag img
+    const img = document.querySelector('#profile img')
+    // substituir a imagem
 
-  const img = document.querySelector('#profile img')
-// substituir a imagem
-
-  if (html.classList.contains('light')) {
-    // se tiver light mode, adicionar a imagem light
-img.setAttribute('src', './assets/A2.png')}
-    else {
-//se tiver sem light toggleMode, manter a imagem normal
-img.setAttribute('src', './assets/Avatar.png')}
-
-  
-  }
+    if (html.classList.contains('light')) {
+      // se tiver light mode, adicionar a imagem light
+      img.setAttribute('src', './assets/A2.png')
+    } else {
+      //se tiver sem light toggleMode, manter a imagem normal
+      img.setAttribute('src', './assets/Avatar.png')
+    }
+}
